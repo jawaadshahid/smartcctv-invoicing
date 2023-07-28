97a99591e292aac50aae4bfc923a7ebd5dd42126
Jawaad Shahid <37578490+jawaadshahid@users.noreply.github.com>
2023-07-28 16:24:33 +0100

    Delete public/build directory

--- public/build/routes/quotes-FZKCCOQC.js
+++
@@ -1,1 +0,0 @@
-import{a as f}from"/build/_shared/chunk-4P2AEKIR.js";import{F as e,I as l,J as r}from"/build/_shared/chunk-XTGBAL5E.js";import{e as t}from"/build/_shared/chunk-ADMCF34Z.js";l();var o=t(r());function d(){return(0,o.jsx)("div",{className:f,children:(0,o.jsx)(e,{})})}export{d as default};
