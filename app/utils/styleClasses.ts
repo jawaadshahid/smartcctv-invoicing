53a65958f46cd963f56feee7889678729df818af
jawaadshahid <jawaadbshahid@hotmail.com>
2024-08-26 12:13:41 +0100

    fixes table width exceeding bounds

--- app/utils/styleClasses.ts
+++ app/utils/styleClasses.ts
@@ -4,5 +4,5 @@ export const formClass = "bg-base-300 px-4 py-2 rounded-lg";
 export const inputClass = "input input-bordered w-full";
 export const selectClass = "select select-bordered w-full";
 export const respTRClass = "flex flex-col md:table-row";
-export const TDClass = "bg-transparent border-0";
+export const TDClass = "bg-transparent border-0 whitespace-normal";
 export const respTDClass = `${TDClass} before:content-[attr(data-label)] before:block before:mb-1 md:before:hidden`;
