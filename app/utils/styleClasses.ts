871a7790bd1a2164d5f9da9ebcc0a2f8b5277fc6
jawaadshahid <jawaadbshahid@hotmail.com>
2024-10-03 09:38:11 +0100

    reapplies table cell bg styling

--- app/utils/styleClasses.ts
+++ app/utils/styleClasses.ts
@@ -5,5 +5,5 @@ export const inputClass = "input input-bordered w-full";
 export const selectClass = "select select-bordered w-full";
 export const respTRClass = "relative block px-4 py-2 md:p-0 md:table-row min-h-16 md:min-h-0 text-pretty";
 export const TDClass = "border-b-0 whitespace-normal leading-5";
-export const respTDClass = `${TDClass} inline md:table-cell p-0 md:p-4 empty:hidden md:empty:table-cell before:content-[attr(data-label)] md:before:hidden`;
+export const respTDClass = `${TDClass} inline md:table-cell p-0 md:p-4 bg-transparent empty:hidden md:empty:table-cell before:content-[attr(data-label)] md:before:hidden`;
 export const respMidTDClass = `${respTDClass} border-r border-inherit md:border-none pr-2 mr-2`;
